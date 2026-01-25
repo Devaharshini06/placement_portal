7eb7de53cc091f44a3a89568f80003404a61caf1
Devaharshini06 <devaharshinivootakoti@gmail.com>
2026-01-25 14:32:06 +0530

    Initial commit

---
+++ placement_portal_23f3001285/backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from config import Config
+from models import db
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    JWTManager(app)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ placement_portal_23f3001285/backend/config.py
@@ -0,0 +1,9 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = "ppa-v2-secret"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "ppa.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = "ppa-v2-jwt-secret"
